d914cdba034b983c26e6b36a1c5b33a9c4e993d6
Shinikii <billmingko@yahoo.com>
2018-09-30 23:07:45 -0400

    Add files via upload
    Observer Pattern Scripts Implemented

---
+++ 3DEngine1/3DEngine1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+       
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        rb.AddForce(movement * speed);
+
+      
+        if (Input.GetButton("Fire1") == true)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
